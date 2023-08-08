fe18eee1f26889de1f2244cbaf112cfe1d990bfa
HamO001 <ndarugastiv9@gmail.com>
2023-08-08 02:33:48 -0700

    focused

---
+++ 0x00-python-hello_world/5-print_string.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Holberton School"
+my_string = str[:9]
+print(str * 3)
+print(my_string)
---
+++ 0x00-python-hello_world/6-concat.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str1 = "Holberton"
+str2 = "School"
+#my codes comes in here
+print(f"welcome to {str1} {str2}!")
