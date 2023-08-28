3b3c29611c4f53c4f9c32165c1ab5508c4b942c4
HamO001 <ndarugastiv9@gmail.com>
2023-08-28 07:33:03 -0700

    fixed

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 0-safe_print_list.py
+
+def safe_print_list(my_list=[], x=0):
+    count = 0
+
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+            count += 1
+    except IndexError:
+        pass
+    print()
+    return count
