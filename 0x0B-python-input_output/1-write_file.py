a8e8bb56d5f3f3574446f39e71d1c27f115c1b20
HamO001 <ndarugastiv9@gmail.com>
2023-09-12 12:42:15 -0700

    weeh

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines a file-writing function."""
+
+
+def write_file(filename="", text=""):
+    """Write a string to a UTF8 text file.
+
+    Args:
+        filename (str): The name of the file to write.
+        text (str): The text to write to the file.
+    Returns:
+        The number of characters written.
+    """
+    try:
+        with open(filename, "w", encoding="utf-8") as f:
+            return f.write(text)
+    except Exception as e:
+        print(f"Error writing to file: {e}")
+        return 0
